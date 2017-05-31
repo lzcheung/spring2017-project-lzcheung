151f6fdc6356ee5cbe59d481c2455279160506f9
Liang Cheung <lzcheung@calpoly.edu>
2017-05-31 00:00:05 -0700

    added first scene

---
+++ src/main/java/ui/ImportFileScene.java
@@ -0,0 +1,72 @@
+package main.java.ui;
+
+import java.io.File;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import main.java.controller.ControllerManager;
+import main.java.controller.DataSingleton;
+import main.java.controller.FileProcessor;
+
+public class ImportFileScene extends Scene {
+  
+  public static class ModifiedVBox extends VBox {
+    public ModifiedVBox(final Stage stage) {
+      super(12);
+      final FileChooser fileChooser = new FileChooser();
+
+      final Button openButton = new Button("Select Excel file.");
+
+      openButton.setOnAction(
+          new EventHandler<ActionEvent>() {
+              @Override
+              public void handle(final ActionEvent e) {
+                File file = fileChooser.showOpenDialog(stage);
+                if (file != null && file.getName().endsWith(".xlsx")) {
+                  System.out.println(file.getParent());
+                  String path = file.getAbsolutePath();
+                  
+                  FileProcessor fp = new FileProcessor();
+                  DataSingleton state = DataSingleton.getInstance();
+                  ControllerManager controller = new ControllerManager();
+                  
+                  fp.readFile(path, state);
+                  
+                  controller.addRoom("117/117", 50);
+                  controller.addRoom("224/224", 16);
+                  controller.addRoom("228/227", 13);
+                  controller.addRoom("125/122", 17);
+                  controller.addRoom("UU220", 40);
+                  
+                  controller.seatExams(state.getExams());
+                  
+                  controller.printAllRooms();
+                }
+              }
+          });
+      
+      GridPane inputGridPane = new GridPane();
+      
+      GridPane.setConstraints(openButton, 0, 0);
+      
+      inputGridPane.setHgap(20);
+      inputGridPane.setVgap(20);
+      inputGridPane.getChildren().addAll(openButton);
+      
+      this.getChildren().addAll(inputGridPane);
+      this.setPadding(new Insets(12, 12, 12, 12));
+    }
+  }
+  
+  public ImportFileScene(final Stage stage) {
+    super(new ModifiedVBox(stage));
+  }
+
+}
--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -1,21 +1,7 @@
 package main.java.ui;
 
-import java.io.File;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-import main.java.controller.ControllerManager;
-import main.java.controller.DataSingleton;
-import main.java.controller.FileProcessor;
 
 public class Main extends Application {
   
@@ -24,62 +10,9 @@ public class Main extends Application {
   }
   
   @Override
-  public void start(final Stage stage) {
-    stage.setTitle("File Chooser Sample");
-
-    final FileChooser fileChooser = new FileChooser();
-
-    final Button openButton = new Button("Select Excel file.");
-
-    openButton.setOnAction(
-        new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(final ActionEvent e) {
-              File file = fileChooser.showOpenDialog(stage);
-              if (file != null && file.getName().endsWith(".xlsx")) {
-                System.out.println(file.getParent());
-                String path = file.getAbsolutePath();
-                
-                FileProcessor fp = new FileProcessor();
-                DataSingleton state = new DataSingleton();
-                ControllerManager controller = new ControllerManager();
-                
-                fp.readFile(path, state);
-                // state.printExams();
-                
-                controller.addRoom("117/117", 50);
-                controller.addRoom("224/224", 16);
-                controller.addRoom("228/227", 13);
-                controller.addRoom("125/122", 17);
-                controller.addRoom("UU220", 40);
-                
-                // state.printExams();
-                
-                controller.seatExams(state.getExams());
-                
-                controller.printAllRooms();
-                
-                String outPath = fp.generateOutPath(file.getParent(), file.getName());
-                System.out.println(outPath);
-                fp.writeToExcelFile(outPath, state.getExams());
-              }
-            }
-        });
-
-    final GridPane inputGridPane = new GridPane();
-
-    GridPane.setConstraints(openButton, 0, 0);
-    
-    inputGridPane.setHgap(6);
-    inputGridPane.setVgap(6);
-    inputGridPane.getChildren().addAll(openButton);
-
-    final Pane rootGroup = new VBox(12);
-    rootGroup.getChildren().addAll(inputGridPane);
-    rootGroup.setPadding(new Insets(12, 12, 12, 12));
-
-    stage.setScene(new Scene(rootGroup));
-    stage.show();
+  public void start(final Stage window) {
+    window.setTitle("File Chooser Sample");
+    window.setScene(new ImportFileScene(window));
+    window.show();
   }
-  
 }
